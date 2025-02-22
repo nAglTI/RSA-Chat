7a2f34171d73f32a6fd285591ae2e30281ba435b
hypergonial <git@hypergonial.com>
2025-02-22 18:41:11 +0100

    Update markdown renderer

--- composeApp/src/commonMain/kotlin/com/hypergonial/chat/view/content/ChannelContent.kt
+++ composeApp/src/commonMain/kotlin/com/hypergonial/chat/view/content/ChannelContent.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
